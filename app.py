805f6720470c041a0f9187705410e19ca0f0c750
Pratyush Dwivedi <pratzyushd@gmail.com>
2020-10-28 13:46:04 +0400

    Create basic website
    Created basic website UI using Bootstrap. Created placeholder cards for each of the search and sorting types.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("home.html")
 
 @app.route("/hello")
 @app.route("/hello/<name>")
